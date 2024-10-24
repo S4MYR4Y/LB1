12a0df28ecf9a620f90bb158262bed28cd767f5c
Yaroslav <ykogogin@ukr.net>
2024-10-24 16:00:05 +0300

    task 4

--- Telegram API.py
+++ Telegram API.py
@@ -1,16 +1,28 @@
-from telethon import TelegramClient, events
+#from telethon import TelegramClient, events
 
+
+
+#bot = TelegramClient('my_bot78', api_id, api_hash).start()
+
+
+#@bot.on(events.NewMessage(pattern='Yarik'))
+#async def start(event):
+ #   await event.respond('Hello')
+
+
+#def main():
+
+ #   bot.run_until_disconnected()
+
+from telethon import TelegramClient, events
 api_id = 23065060
 api_hash = 'b846429cd65f033b0a6a14f3fcfcd2ca'
+client = TelegramClient('anon', api_id, api_hash)
 
-bot = TelegramClient('gooam', api_id, api_hash).start(bot_token=api_hash)
+@client.on(events.NewMessage)
+async def my_event_handler(event):
+    if 'hello' in event.raw_text:
+        await event.reply('hi!')
 
-
-@bot.on(events.NewMessage(pattern='/Start'))
-async def start(event):
-    await event.respond('Hello')
-
-
-def main():
-
-    bot.run_until_disconnected()
+client.start()
+client.run_until_disconnected()
